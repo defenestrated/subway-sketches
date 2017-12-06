5c01568e9be5abfc9018848c1d81b728b28ecea4
Sam Galison <samface@gmail.com>
2017-12-06 01:16:23 -0500

    zoom added, finally understanding npm

--- index.js
+++ index.js
@@ -6,35 +6,46 @@ var topojson = require('topojson')
 
 var debug = true
 
-var app = app || {}
-
-app.status = {
-  routesloaded: false,
-  stationsloaded: false,
-  maploaded: false
+var app = {
+  status:{
+    routesloaded: false,
+    stationsloaded: false,
+    maploaded: false
+  },
+  parchment: d3.select(".parchment")
 }
 
 loaddata()
+
+app.parchment.call(d3.zoom()
+                   .scaleExtent([1 / 2, 5])
+                   .on("zoom", zoomed));
+
+function zoomed() {
+  var g = d3.select("g.map-data")
+  g.attr("transform", d3.event.transform)
+}
 
 function render() {
   app.vw = window.innerWidth
   app.vh = window.innerHeight
   if (debug) console.log(app.vw, app.vh);
 
-  var parchment = d3.select(".parchment");
+  // var parchment = d3.select(".parchment");
 
   // var subunits = topojson.feature(app.mapdata, app.mapdata.objects.subunits);
   var projection = d3.geoMercator()
       .center([-73.9536162, 40.7361])
+      // .fitSize([500,500], app.mapdata.objects.subway_lines)
 		  .scale(100000)
 		  .translate([app.vw/2,app.vh/2])
 
   var mappath = d3.geoPath()
       .projection(projection)
 
-  var mapstuff = parchment.append("g").attr("class", "map-data")
+  var mapstuff = app.parchment.append("g").attr("class", "map-data")
 
-  // parchment.append("path")
+  // app.parchment.append("path")
   //     .datum(subunits)
   //     .attr("d", path);
 
@@ -49,8 +60,12 @@ function render() {
 
 
 function loaddata() {
-  var lines_loc = window.location.protocol + "//" + window.location.host + "/assets/data/subway-lines.topojson"
-  var stops_loc = window.location.protocol + "//" + window.location.host + "/assets/data/subway-stations.topojson"
+  // var lines_loc = window.location.protocol + "//" + window.location.host + "/assets/data/subway-lines.topojson"
+  // var stops_loc = window.location.protocol + "//" + window.location.host + "/assets/data/subway-stations.topojson"
+  var lines_loc = "assets/data/subway-lines.topojson"
+  var stops_loc = "assets/data/subway-stations.topojson"
+
+  console.log(lines_loc)
 
   d3.json(lines_loc, function(error, stuff) {
     if (error) return console.error(error)
